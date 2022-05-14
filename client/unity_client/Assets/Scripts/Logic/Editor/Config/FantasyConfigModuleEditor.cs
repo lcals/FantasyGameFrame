84184b27c8ba735b36792eb8c1441daf412a2699
lcals <lcalswin@gmail.com>
2022-05-14 10:03:43 +0800

    move files

---
+++ client/unity_client/Assets/Scripts/Logic/Editor/Config/FantasyConfigModuleEditor.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+
+namespace Fantasy.Logic.Editor
+{
+    public static class FantasyConfigModuleEditor 
+    {
+        [MenuItem("FantasyTools/GenerateDataStruct")]
+        public static void GenerateDataStruct()
+        {
+            
+        }
+
+       
+       
+    }
+}
