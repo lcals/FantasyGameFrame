c86a520375e940a5193af59b2d7b7c1f3c59f7ef
lcals <lcalswin@gmail.com>
2022-05-14 19:08:30 +0800

    fix gen files

--- client/unity_client/Assets/Scripts/Logic/Editor/Config/Tools/mac_gen.sh
+++ client/unity_client/Assets/Scripts/Logic/Editor/Config/Tools/mac_gen.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo "mac_gen.sh"
 cd Assets/Scripts/Logic/Editor/Config/Tools || exit 1
+dir="./../../../Runtime/Config/Gen/"
+for dir_del in $dir/* ; do
+ if [ -d $dir_del ]; then
+  rm -rf $dir_del
+ fi
+done
 ./flatc  --csharp   --csharp --cs-gen-json-serializer  --gen-object-api    -o ./../../../Runtime/Config/Gen  ./../Fbs/ConfigRoot.fbs ./../Fbs/Role.fbs ./../Fbs/Item.fbs
