9a2d4c1e3e46e3b189463ba0894ed002025113ce
lcals <lcalswin@gmail.com>
2022-05-24 20:12:40 +0800

    To serve non-standard content type

--- tools/static_file_server/Program.cs
+++ tools/static_file_server/Program.cs
@@ -1,5 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDirectoryBrowser();
 var app = builder.Build();
-app.UseFileServer(enableDirectoryBrowsing: true);
+app.UseFileServer(new FileServerOptions()
+{
+    EnableDirectoryBrowsing = true,
+    StaticFileOptions =
+    {
+        ServeUnknownFileTypes = true,
+        DefaultContentType = "application/octet-stream"
+    }
+});
 app.Run();
